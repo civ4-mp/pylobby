3ac98c2ac45eca065a5f0ddf749475e5ddfde1e6
Zulan <git@zulan.net>
2016-07-07 12:35:08 +0200

    Add civ4col gamekey

--- gs_consts.py
+++ gs_consts.py
@@ -11,7 +11,14 @@ HAS_FULL_RULES_FLAG = 128
 
 ghchal = """\x41\x43\x4E\x2B\x78\x38\x44\x6D\x57\x49\x76\x6D\x64\x5A\x41\x51\x45\x37\x68\x41\x00"""
 
-gngk = {'civ4': 'y3D9Hw', 'civ4bts': 'Cs2iIq', 'civ4btsjp': 'Cs2iIq', 'gmtest': 'HA6zkS'}
+#gamename-gamekey
+gngk = {'civ4':        'y3D9Hw',
+        'civ4bts':     'Cs2iIq',
+        'civ4btsjp':   'Cs2iIq',
+        'civ4colpc':   '2yheDS',
+        'civ4coljp':   '5wddmt',
+        'gmtest':      'HA6zkS',
+        'gslive':      'Xn221z'}
 
 defaultfields = ['hostname', 'gamemode', 'hostname', 'gamever', 'passwd', 'hostport', 'staging', 'newgame', 'mapname',
                  'gametype', 'mynumplayers', 'maxnumplayers', 'nummissing', 'pitboss']
